437df4ca6ff7017f23acbda0638cf1bb8f2ae9ad
NightSkumbry <night_skumbry@outlook.com>
2026-09-18 19:42:12 +0300

    Initial commit

---
+++ src/test/java/com/p3319/lab1/Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p3319.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
